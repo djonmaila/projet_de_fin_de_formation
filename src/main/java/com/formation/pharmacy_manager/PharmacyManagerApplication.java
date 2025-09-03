7e00fad193592c5522dc6b5141e1689738718236
Djong-dev <djonmailapythagore@gmail.com>
2025-09-04 00:56:27 +0100

    premier commit

---
+++ src/main/java/com/formation/pharmacy_manager/PharmacyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.formation.pharmacy_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyManagerApplication.class, args);
+	}
+
+}
