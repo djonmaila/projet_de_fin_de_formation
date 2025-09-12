43c18fca1595c9c416e9437ad4bb9596f9cfa09e
Patrick Thadeus <patrickichu058@gmail.com>
2025-09-12 04:26:20 +0100

    set the payment status

--- src/main/java/com/formation/pharmacy_manager/dto/paymentDto/paymentRequestDto.java
+++ src/main/java/com/formation/pharmacy_manager/dto/paymentDto/paymentRequestDto.java
@@ -1,24 +1,17 @@
 package com.formation.pharmacy_manager.dto.paymentDto;
 
+import lombok.AllArgsConstructor;
 // import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 
 public class PaymentRequestDto {
     private double totalAmount;
     private String paymentMethod;
     private long commandId;
-
-    public PaymentRequestDto(double totalAmount, String paymentMethod, long commandId)
-    {
-        this.totalAmount = totalAmount;
-        this.paymentMethod = paymentMethod;
-        this.commandId = commandId;
-    }
-
-    
 }
