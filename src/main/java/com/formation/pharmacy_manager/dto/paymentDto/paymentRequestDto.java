8501dcb7c797616e7fc819042540817e196c3229
Patrick Thadeus <patrickichu058@gmail.com>
2025-09-12 04:32:12 +0100

    set the payment reauest dto

--- src/main/java/com/formation/pharmacy_manager/dto/paymentDto/paymentRequestDto.java
+++ src/main/java/com/formation/pharmacy_manager/dto/paymentDto/paymentRequestDto.java
@@ -1,17 +1,17 @@
 package com.formation.pharmacy_manager.dto.paymentDto;
 
+import com.formation.pharmacy_manager.entities.PaymentMethod;
+
 import lombok.AllArgsConstructor;
 // import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Data
 
 public class PaymentRequestDto {
-    private double totalAmount;
-    private String paymentMethod;
     private long commandId;
+    private PaymentMethod paymentMethod;
 }
